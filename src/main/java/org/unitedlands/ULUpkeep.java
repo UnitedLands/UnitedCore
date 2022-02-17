286c32dffb22fa4f69c011e812ca4e4165993d88
Maroon28 <74205260+Maroon28@users.noreply.github.com>
2022-02-17 08:13:56 +0200

    Add Bonus Discount This is a first attempt at adding bonus discounts into the upkeep plugin. Essentially, the way it works is that you bonus blocks that you are given are considered 'free', I.E you don't pay anything for them.
    For example, if you're given 10 bonus blocks after an event or similar, you'll pay nothing for those 10 claims, and they'll be counted as a 'discount'. The price of them will dynamically change according to how much each plot costs as well.

--- src/main/java/org/unitedlands/ULUpkeep.java
+++ src/main/java/org/unitedlands/ULUpkeep.java
@@ -3,6 +3,7 @@ package org.unitedlands;
 import com.palmergames.adventure.text.Component;
 import com.palmergames.adventure.text.TextComponent;
 import com.palmergames.bukkit.towny.event.NationUpkeepCalculationEvent;
+import com.palmergames.bukkit.towny.event.TownClaimEvent;
 import com.palmergames.bukkit.towny.event.TownUpkeepCalculationEvent;
 import com.palmergames.bukkit.towny.event.statusscreen.TownStatusScreenEvent;
 import com.palmergames.bukkit.towny.object.Nation;
@@ -27,7 +28,9 @@ public class ULUpkeep extends JavaPlugin implements Listener {
     @EventHandler
     public void calculateTownUpkeepEvent(TownUpkeepCalculationEvent event) {
         Town town = event.getTown();
-        double upkeep = UpkeepCalculators.calculateTownUpkeep(config, town, true);
+        double bonusDiscount = UpkeepCalculators.calculateBonusDiscount(config, town);
+        double upkeep = Math.abs(UpkeepCalculators.calculateTownUpkeep(config, town, true) - bonusDiscount);
+
         event.setUpkeep(upkeep);
     }
 
@@ -45,32 +48,45 @@ public class ULUpkeep extends JavaPlugin implements Listener {
         Town town = event.getTown();
         int townsize = town.getTownBlocks().size();
 
-        double upkeep = UpkeepCalculators.calculateTownUpkeep(config, town, false);
-        double discountedUpkeep = UpkeepCalculators.calculateTownUpkeep(config, town, true);
+        double undiscountedUpkeep = UpkeepCalculators.calculateTownUpkeep(config, town, false);
+        double upkeepWithNationDiscount = UpkeepCalculators.calculateTownUpkeep(config, town, true);
 
-        double discount = Math.floor(upkeep - discountedUpkeep);
+        double nationDiscount = undiscountedUpkeep - upkeepWithNationDiscount;
+        double bonusDiscount = UpkeepCalculators.calculateBonusDiscount(config, town);
 
+        double discountedUpkeep = Math.abs(upkeepWithNationDiscount - bonusDiscount);
 
+        getServer().broadcastMessage("Discount: " + nationDiscount);
         // Loop through the components in the status screen
         screenComponents.forEach(component -> {
             var componentContent = component.content();
-
-
-            if (componentContent.contains("upkeep")) {
-                // checking if there's a discount to begin with
-                if (UpkeepCalculators.calculateNationDiscount(town) > 0) {
-                    // Define our custom component, and replace the default one with it.
-                    Component upkeepComponent = Component.text("§2Daily Upkeep: §c" + discountedUpkeep + " Gold" + " §b[Nation Discount: §6" + discount + " Gold§b]");
-                    screen.replaceComponent("upkeep", (TextComponent) upkeepComponent);
-                }
-            }
-
             // Check if the component is the town size
             if (componentContent.contains("Town Size")) {
 
                 // Replace towny's default component with our new one, without the slashes in it
                 Component newComponent = Component.text("\n§2Town Size: §a" + townsize);
                 screen.replaceComponent("townblocks", (TextComponent) newComponent);
+            }
+
+            if (componentContent.contains("Upkeep")) {
+                Component upkeepComponent;
+                // checking if there's a discount to begin with
+
+                if (nationDiscount > 0 && bonusDiscount > 0) {
+                    upkeepComponent = Component.text("§2Upkeep: §7§m" + undiscountedUpkeep + "§r§c " +
+                            discountedUpkeep + " Gold" + " §2[Bonus Discount: §a" + bonusDiscount + " Gold§2]" +
+                            " [Nation Discount: §a" + nationDiscount +" Gold§2]");
+                    screen.replaceComponent("upkeep", (TextComponent) upkeepComponent);
+                } else if (nationDiscount > 0) {
+                    // Define our custom component, and replace the default one with it.
+                    upkeepComponent = Component.text("§2Upkeep: §7§m" + undiscountedUpkeep + "§r§c " + discountedUpkeep +
+                            " Gold" + " §2[Nation Discount: §a" + nationDiscount + " Gold§2]");
+                    screen.replaceComponent("upkeep", (TextComponent) upkeepComponent);
+                } else if (bonusDiscount != 0) {
+                    upkeepComponent = Component.text("§2Upkeep: §7§m" + undiscountedUpkeep + "§r§c " + discountedUpkeep +
+                            " Gold" + " §2[Bonus Discount: §a" + bonusDiscount + " Gold§2]");
+                    screen.replaceComponent("upkeep", (TextComponent) upkeepComponent);
+                }
             }
         });
 
--- src/main/java/org/unitedlands/UpkeepCalculators.java
+++ src/main/java/org/unitedlands/UpkeepCalculators.java
@@ -5,7 +5,7 @@ import com.palmergames.bukkit.towny.object.Town;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class UpkeepCalculators {
-    public static double calculateTownUpkeep(FileConfiguration config, Town town, boolean discount) {
+    public static double calculateTownUpkeep(FileConfiguration config, Town town, boolean nationDiscount) {
 
         int baseUpkeepPrice = config.getInt("town.baseUpkeepPrice");
         int townPlotCount = town.getTownBlocks().size();
@@ -26,14 +26,14 @@ public class UpkeepCalculators {
 
         double upkeepPerPlot;
 
-        if (discount) {
+        if (nationDiscount) {
             upkeepPerPlot = (baseUpkeepPrice * riseMod) / (fallMod + calculateNationDiscount(town));
 
         } else {
             upkeepPerPlot = (baseUpkeepPrice * riseMod) / fallMod;
         }
 
-        double upkeep = Math.floor(upkeepPerPlot * townPlotCount);
+        double upkeep = Math.floor((upkeepPerPlot * townPlotCount));
 
         return upkeep;
 
@@ -68,7 +68,7 @@ public class UpkeepCalculators {
         return upkeep;
     }
 
-    public static double calculateNationDiscount(Town town) {
+    private static double calculateNationDiscount(Town town) {
 
         if (!town.hasNation()) {
             return 0;
@@ -80,12 +80,25 @@ public class UpkeepCalculators {
             return 0;
         }
 
-        double claimContributionPercent = (double) town.getTownBlocks().size() / nation.getTownBlocks().size();
-        double residentContributionPercent = (double) town.getNumResidents() / nation.getNumResidents();
+        double claimContributionPercent = (double) town.getTownBlocks().size() / (double) nation.getTownBlocks().size();
+        double residentContributionPercent = (double) town.getNumResidents() / (double) nation.getNumResidents();
 
         double nationDiscountModifier = residentContributionPercent + claimContributionPercent;
 
         return nationDiscountModifier;
+    }
+
+    public static double calculateBonusDiscount(FileConfiguration config, Town town) {
+
+        int totalBlocks = town.getTownBlocks().size();
+        int bonusBlocks = town.getBonusBlocks();
+        double upkeepPerPlot = Math.floor(calculateTownUpkeep(config, town, false) / totalBlocks);
+
+        if (bonusBlocks == 0) {
+            return 0;
+        }
+        return bonusBlocks * upkeepPerPlot;
+
     }
 
 }
