dce660932b494621286bd867293eb6a87d78928e
Maroon28 <74205260+Maroon28@users.noreply.github.com>
2022-03-21 15:36:23 +0200

    Properly add UnitedUpkeep to the repository Unfortunately, I made some erros with git that merged the repository with the main repo, which messed up a lot of stuff. The commit history for UnitedUpkeep will be all over the place now, but its finally in the project now though

---
+++ UnitedUpkeep/src/main/java/org/unitedlands/upkeep/calculators/TownUpkeepCalculator.java
@@ -0,0 +1,104 @@
+package org.unitedlands.upkeep.calculators;
+
+import com.palmergames.bukkit.towny.object.Nation;
+import com.palmergames.bukkit.towny.object.Town;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.unitedlands.upkeep.UnitedUpkeep;
+
+public class TownUpkeepCalculator {
+
+    private final UnitedUpkeep unitedUpkeep;
+    private final Town town;
+
+    public TownUpkeepCalculator(UnitedUpkeep unitedUpkeep, Town town) {
+        this.unitedUpkeep = unitedUpkeep;
+        this.town = town;
+    }
+
+    public double calculateNationDiscountedTownUpkeep() {
+        double upkeepPerPlot = (getBaseTownUpkeepPrice() * getRiseMod()) / (getFallMod() + calculateNationDiscount());
+        return Math.floor((upkeepPerPlot * getTownPlotCount()));
+
+    }
+
+    public double calculateTownUpkeep() {
+        double upkeepPerPlot = (getBaseTownUpkeepPrice() * getRiseMod()) / getFallMod();
+        return Math.floor((upkeepPerPlot * getTownPlotCount()));
+    }
+
+    public double calculateBonusBlockDiscount() {
+
+        int totalBlocks = getTownPlotCount();
+        int bonusBlocks = town.getBonusBlocks();
+        double upkeepPerPlot = Math.floor(calculateTownUpkeep() / totalBlocks);
+
+        if (bonusBlocks == 0) {
+            return 0;
+        }
+
+        if (bonusBlocks >= totalBlocks) {
+            return calculateNationDiscountedTownUpkeep();
+        }
+
+        return bonusBlocks * upkeepPerPlot;
+
+    }
+
+    private double getFallMod() {
+        int townResidentCount = town.getNumResidents();
+
+        double fallStep = getConfiguration().getDouble("town.fallStep");
+        int fallAt = getConfiguration().getInt("town.fallAt");
+
+        int townResidentStep = townResidentCount / fallAt;
+
+        return (fallStep * townResidentStep) + 1;
+    }
+
+    private int getTownPlotCount() {
+        return town.getTownBlocks().size();
+    }
+
+    private double getRiseMod() {
+        int townPlotCount = getTownPlotCount();
+
+        int riseAt = getConfiguration().getInt("town.riseAt");
+        double riseStep = getConfiguration().getDouble("town.riseStep");
+
+        int townPlotStep = townPlotCount / riseAt;
+
+        return (riseStep * townPlotStep) + 1;
+    }
+
+    private double calculateNationDiscount() {
+        if (!town.hasNation()) {
+            return 0;
+        }
+        Nation nation = town.getNationOrNull();
+        if (nation.getNumTowns() == 1) {
+            return 0;
+        }
+
+        double claimContributionPercent = (double) getTownPlotCount() / nation.getTownBlocks().size();
+        double residentContributionPercent = (double) town.getNumResidents() / nation.getNumResidents();
+
+        return residentContributionPercent + claimContributionPercent;
+    }
+
+    private int getBaseTownUpkeepPrice() {
+        return getConfiguration().getInt("town.baseUpkeepPrice");
+    }
+
+    private FileConfiguration getConfiguration() {
+        return unitedUpkeep.getConfig();
+    }
+
+    public double getNationDiscount() {
+        return calculateTownUpkeep() - calculateNationDiscountedTownUpkeep();
+    }
+
+    public double getDiscountedUpkeep() {
+        return Math.abs(calculateNationDiscountedTownUpkeep() - calculateBonusBlockDiscount());
+    }
+
+}
---
+++ UnitedUpkeep/src/main/java/org/unitedlands/upkeep/listeners/StatusScreenListener.java
@@ -0,0 +1,103 @@
+package org.unitedlands.upkeep.listeners;
+
+import com.palmergames.adventure.text.Component;
+import com.palmergames.adventure.text.TextComponent;
+import com.palmergames.adventure.text.format.NamedTextColor;
+import com.palmergames.adventure.text.format.TextDecoration;
+import com.palmergames.bukkit.towny.event.statusscreen.TownStatusScreenEvent;
+import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.statusscreens.StatusScreen;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.unitedlands.upkeep.UnitedUpkeep;
+import org.unitedlands.upkeep.calculators.TownUpkeepCalculator;
+
+public class StatusScreenListener implements Listener {
+    private final UnitedUpkeep unitedUpkeep;
+    private Town town;
+    private StatusScreen screen;
+
+    public StatusScreenListener(UnitedUpkeep unitedUpkeep) {
+        this.unitedUpkeep = unitedUpkeep;
+    }
+
+    @EventHandler
+    public void sendStatusScreen(TownStatusScreenEvent event) {
+        screen = event.getStatusScreen();
+        town = event.getTown();
+        replaceTownSizeComponent();
+        replaceUpkeepComponent();
+    }
+
+    private TownUpkeepCalculator getTownUpkeepCalculator() {
+        return new TownUpkeepCalculator(unitedUpkeep, town);
+    }
+
+    private int getTownsize() {
+        return town.getTownBlocks().size();
+    }
+
+    private void replaceUpkeepComponent() {
+        TextComponent upkeepComponent = getUpkeepComponent();
+
+        if (getBonusBlockDiscount() > 0 && getNationDiscount() > 0) {
+            upkeepComponent = getComponentWithAllDiscounts();
+        } else if (getNationDiscount() > 0) {
+            upkeepComponent = upkeepComponent.append(getNationDiscountComponent());
+        } else if (getBonusBlockDiscount() > 0) {
+            upkeepComponent = upkeepComponent.append(getBonusDiscountComponent());
+        }
+
+        screen.replaceComponent("upkeep", upkeepComponent);
+    }
+
+    private TextComponent getComponentWithAllDiscounts() {
+        return Component.text("\n")
+                .append(getUpkeepComponent())
+                .append(getNationDiscountComponent())
+                .append(getBonusDiscountComponent());
+    }
+
+    private TextComponent getNationDiscountComponent() {
+        return Component
+                .text("[Nation Discount: ", NamedTextColor.DARK_GREEN)
+                .append(Component.text(getNationDiscount() + " Gold", NamedTextColor.GREEN))
+                .append(Component.text("]", NamedTextColor.DARK_GREEN));
+    }
+
+    private TextComponent getBonusDiscountComponent() {
+        return Component
+                .text("[Bonus Discount: ", NamedTextColor.DARK_GREEN)
+                .append(Component.text(getBonusBlockDiscount() + " Gold", NamedTextColor.GREEN))
+                .append(Component.text("]", NamedTextColor.DARK_GREEN));
+    }
+
+    private TextComponent getUpkeepComponent() {
+        var calculator = getTownUpkeepCalculator();
+        double upkeep = calculator.calculateTownUpkeep();
+        double discountedUpkeep = calculator.getDiscountedUpkeep();
+        return Component
+                .text("\n")
+                .append(Component.text("\nUpkeep: ", NamedTextColor.DARK_GREEN))
+                .append(Component.text( " " + upkeep, NamedTextColor.GRAY, TextDecoration.STRIKETHROUGH))
+                .append(Component.text( discountedUpkeep + " Gold", NamedTextColor.RED));
+    }
+
+    private void replaceTownSizeComponent() {
+        TextComponent townSizeComponent = Component
+                .text("\n")
+                .append(Component.text("\nTown Size: ", NamedTextColor.DARK_GREEN)
+                .append(Component.text(getTownsize(), NamedTextColor.GREEN)));
+        screen.replaceComponent("townblocks", townSizeComponent);
+    }
+
+    private double getNationDiscount() {
+        var calculator = getTownUpkeepCalculator();
+        return calculator.getNationDiscount();
+    }
+
+    private double getBonusBlockDiscount() {
+        var calculator = getTownUpkeepCalculator();
+        return calculator.calculateBonusBlockDiscount();
+    }
+}
