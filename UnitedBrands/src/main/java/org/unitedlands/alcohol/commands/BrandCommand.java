eb1b93a1a948d29e32784f5b58f82fd055b29f6c
Maroon28 <74205260+Maroon28@users.noreply.github.com>
2022-03-26 20:53:04 +0200

    UnitedBrands - v2.0 Can finally step out of beta stages now. The plugin is a lot more clean and stable than it used to be. - Added tons of sanity checks. There were a bunch of ways to mess up the plugin using commands with missing arguments, or supplying dumb arguments. This is now all dealt with properly - Extracted a lot of methods from onCommand and made it a switch statement instead. This is a bit cleaner than what it used to be, and now its easier to modify and work with the command for any future things needed - Added a few more getMessage util commands with different arguments. - Fixed <player> placeholder in messages - Tons of tiny bug fixes that came up while cleaning the code!

--- UnitedBrands/src/main/java/org/unitedlands/alcohol/commands/BrandCommand.java
+++ UnitedBrands/src/main/java/org/unitedlands/alcohol/commands/BrandCommand.java
@@ -1,6 +1,7 @@
 package org.unitedlands.alcohol.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,15 +14,19 @@ import org.unitedlands.alcohol.brand.Brand;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class BrandCommand implements CommandExecutor {
     private final UnitedBrands unitedBrands;
+    private Player player;
     Set<InviteRequest> inviteRequests = new HashSet<>();
 
     public BrandCommand(UnitedBrands unitedBrands) {
         this.unitedBrands = unitedBrands;
     }
+
+    // TODO- Add tab completion
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
@@ -30,181 +35,237 @@ public class BrandCommand implements CommandExecutor {
             return false;
         }
 
-        Player player = (Player) sender;
-        Brand brand = Util.getPlayerBrand(player);
+        player = (Player) sender;
 
-        if (args[0].equals("create")) {
-
-            if (args[1] == null) {
-                player.sendMessage(Util.getMessage("must-specify-brand-name", ""));
-                return true;
-            }
-
-            String[] rawBrandName = removeFirstArgument(args);
-            String brandName = String.join(" ", rawBrandName);
-
-            brand = new Brand(unitedBrands, brandName, player, null);
-            if (Util.hasBrand(player)) {
-                player.sendMessage(Util.getMessage("in-a-brand", Util.getPlayerBrand(player).getBrandName()));
-                return true;
-            }
-
-            if (Util.brandExists(brand)) {
-                player.sendMessage(Util.getMessage("brand-already-exists", brandName));
-                return true;
-            }
-
-            brand.createBrand();
+        if (args.length < 1) {
+            sendHelpMessage();
             return true;
         }
 
-        if (args[0].equals("delete")) {
-
-            brand = Util.getPlayerBrand(player);
-
-            if (brand == null) {
-                player.sendMessage(Util.getMessage("must-own-brand", ""));
-                return true;
-            }
-
-            String brandName = brand.getBrandName();
-
-            if (Util.hasBrand(player) && isBrandOwner(player, brand)) {
-                brand.deleteBrand();
-                player.sendMessage(Util.getMessage("brand-deleted", brandName));
-                return true;
-            }
-            player.sendMessage(Util.getMessage("brand-cannot-be-deleted", brandName));
-            return true;
-        }
-
-        String brandName = "";
-
-        if (Util.hasBrand(player)) {
-            brandName = Util.getPlayerBrand(player).getBrandName();
-        }
-        if (args[0].equals("invite")) {
-
-            if (args[1] == null) {
-                player.sendMessage(Util.getMessage("must-specify-invited-player", ""));
-                return true;
-            }
-
-            Player receiver = Bukkit.getPlayer(args[1]);
-            brand = Util.getPlayerBrand(player);
-
-            if (player == receiver) {
-                player.sendMessage(Util.getMessage("cannot-invite-self", brandName));
-                return true;
-            }
-
-            if (Util.hasBrand(player) && isBrandOwner(player, brand)) {
-                InviteRequest inviteRequest = new InviteRequest(player, receiver);
-                inviteRequests.add(inviteRequest);
-                player.sendMessage(Util.getMessage("player-invited", brandName));
-                receiver.sendMessage(Util.getMessage("brand-invite", brandName));
-                return true;
-            }
-            receiver.sendMessage(Util.getMessage("must-own-brand", ""));
-            return true;
-        }
-
-        if (args[0].equals("accept")) {
-            InviteRequest request = getRequest(player);
-
-            if (request == null) {
-                player.sendMessage(Util.getMessage("no-requests", ""));
-                return true;
-            }
-
-            if (Util.getPlayerBrand(request.getReceiver()) != null) {
-                player.sendMessage(Util.getMessage("already-in-a-brand", ""));
-                return true;
-            }
-
-            brand = Util.getPlayerBrand(request.getSender());
-            brandName = brand.getBrandName();
-            brand.addMember(player);
-
-            request.getSender().sendMessage(Util.getMessage("brand-join-sender", brandName));
-            request.getReceiver().sendMessage(Util.getMessage("brand-join", brandName));
-
-            inviteRequests.remove(request);
-            return true;
-
-        }
-
-        if (args[0].equals("kick")) {
-
-            if (args[1] == null) {
-                player.sendMessage(Util.getMessage("must-specify-kicked-player", ""));
-                return true;
-            }
-
-            if (!isBrandOwner(player, brand)) {
-                player.sendMessage(Util.getMessage("must-own-brand", brandName));
-                return true;
-            }
-            Player kickedPlayer = Bukkit.getPlayer(args[1]);
-
-            if (kickedPlayer == player) {
-                player.sendMessage(Util.getMessage("cannot-kick-self", brandName));
-                return true;
-            }
-
-            brand.removeMember(kickedPlayer);
-            kickedPlayer.sendMessage(Util.getMessage("kicked-from-brand", brandName));
-            player.sendMessage(Util.getMessage("player-kicked", brandName));
-            return true;
-        }
-
-        if (args[0].equals("leave")) {
-            if (isBrandOwner(player, brand)) {
-                player.sendMessage(Util.getMessage("must-delete-brand", brandName));
-                return true;
-            }
-            if (Util.hasBrand(player)) {
-                try {
-                    brand.removeMember(player);
-                } catch (Exception e) {
-                    e.printStackTrace();
+        switch (args[0]) {
+            case "create" -> createBrand(args);
+            case "delete" -> deletePlayerBrand();
+            case "invite" -> {
+                if (args.length == 1) {
+                    player.sendMessage(Util.getMessage("must-specify-invited-player"));
+                    return true;
                 }
-                player.sendMessage(Util.getMessage("brand-leave", brandName));
-                return true;
+                Player inviteReceiver = Bukkit.getPlayer(args[1]);
+                invitePlayerToBrand(inviteReceiver);
             }
-            player.sendMessage(Util.getMessage("must-have-brand", brandName));
-        }
-
-
-        if (args[0].equals("deny")) {
-            InviteRequest request = getRequest(player);
-            if (request != null) {
-                request.getSender().sendMessage(Util.getMessage("brand-deny-sender".replace("<player>", request.getSender().getName())
-                        , brandName));
-                request.getReceiver().sendMessage(Util.getMessage("brand-deny", brandName));
-                inviteRequests.remove(request);
-                return true;
+            case "accept" -> acceptBrandInvite();
+            case "kick" -> {
+                if (args.length == 1) {
+                    player.sendMessage(Util.getMessage("must-specify-kicked-player"));
+                    return true;
+                }
+                kickPlayerFromBrand(Bukkit.getPlayer(args[1]));
             }
-            player.sendMessage(Util.getMessage("no-requests", ""));
-            return true;
-        }
-
-        if (args[0].equals("slogan")) {
-            String[] rawSlogan = removeFirstArgument(args);
-            String slogan = String.join(" ", rawSlogan);
-            brand = new Brand(unitedBrands, brandName, player, null);
-            try {
-                brand.setSlogan(slogan);
-            } catch (Exception e) {
-                e.printStackTrace();
+            case "leave" -> leaveBrand();
+            case "deny" -> denyRequest();
+            case "slogan" -> {
+                Brand brand = Util.getPlayerBrand(player);
+                String slogan = extractMultiWordString(args);
+                changeBrandSlogan(brand, slogan);
             }
-            player.sendMessage(Util.getMessage("slogan-changed", brandName));
-            return true;
+            default -> sendHelpMessage();
         }
 
         return true;
     }
 
+    private void sendHelpMessage() {
+        List<String> helpMessage = unitedBrands.getConfig().getStringList("messages.help-command");
+        for (String message : helpMessage) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
+        }
+    }
+
+    private void createBrand(String[] args) {
+        if (args.length == 1) {
+            player.sendMessage(Util.getMessage("must-specify-brand-name", ""));
+            return;
+        }
+
+        String brandName = String.join(" ", extractMultiWordString(args));
+        Brand brand = new Brand(unitedBrands, brandName, player, null);
+
+        if (Util.hasBrand(player)) {
+            player.sendMessage(Util.getMessage("in-a-brand", Util.getPlayerBrand(player).getBrandName()));
+            return;
+        }
+
+        if (Util.brandExists(brand)) {
+            player.sendMessage(Util.getMessage("brand-already-exists", brandName));
+            return;
+        }
+        brand.createBrand();
+    }
+
+    private void deletePlayerBrand() {
+        Brand brand = Util.getPlayerBrand(player);
+
+        if (brand == null) {
+            player.sendMessage(Util.getMessage("must-own-brand"));
+            return;
+        }
+
+        String brandName = brand.getBrandName();
+
+        if (Util.hasBrand(player) && isBrandOwner(brand)) {
+            brand.deleteBrand();
+            player.sendMessage(Util.getMessage("brand-deleted", brandName));
+            return;
+        }
+
+        player.sendMessage(Util.getMessage("brand-cannot-be-deleted", brandName));
+    }
+
+    private void invitePlayerToBrand(Player inviteReceiver) {
+
+        if (inviteReceiver == null) {
+            player.sendMessage(Util.getMessage("invalid-player"));
+            return;
+        }
+
+        Brand brand = Util.getPlayerBrand(player);
+
+        if (brand == null) {
+            player.sendMessage(Util.getMessage("must-own-brand"));
+            return;
+        }
+
+        String brandName = brand.getBrandName();
+
+        if (player == inviteReceiver) {
+            player.sendMessage(Util.getMessage("cannot-invite-self", brandName));
+            return;
+        }
+
+        if (isBrandOwner(brand)) {
+            InviteRequest inviteRequest = new InviteRequest(player, inviteReceiver);
+            inviteRequests.add(inviteRequest);
+            player.sendMessage(Util.getMessage("player-invited", brandName));
+            inviteReceiver.sendMessage(Util.getMessage("brand-invite", brandName));
+            return;
+        }
+
+        player.sendMessage(Util.getMessage("must-own-brand"));
+
+    }
+
+    private void acceptBrandInvite() {
+        InviteRequest request = getRequest(player);
+
+        if (request == null) {
+            player.sendMessage(Util.getMessage("no-requests"));
+            return;
+        }
+
+        Player receiver = request.getReceiver();
+        Player sender = request.getSender();
+
+        if (Util.getPlayerBrand(receiver) != null) {
+            player.sendMessage(Util.getMessage("already-in-a-brand"));
+            return;
+        }
+
+        Brand brand = Util.getPlayerBrand(sender);
+        String brandName = brand.getBrandName();
+        brand.addMember(player);
+
+        sender.sendMessage(Util.getMessage("brand-join-sender", player));
+        receiver.sendMessage(Util.getMessage("brand-join", brandName));
+
+        inviteRequests.remove(request);
+    }
+
+    private void kickPlayerFromBrand(Player kickedPlayer) {
+
+        if (kickedPlayer == null) {
+            player.sendMessage(Util.getMessage("must-specify-kicked-player", ""));
+            return;
+        }
+
+        Brand brand = Util.getPlayerBrand(player);
+        String brandName = brand.getBrandName();
+
+        if (!isBrandOwner(brand)) {
+            player.sendMessage(Util.getMessage("must-own-brand", brandName));
+            return;
+        }
+
+        if (kickedPlayer == player) {
+            player.sendMessage(Util.getMessage("cannot-kick-self", brandName));
+            return;
+        }
+
+        brand.removeMember(kickedPlayer);
+        kickedPlayer.sendMessage(Util.getMessage("kicked-from-brand", brandName));
+        player.sendMessage(Util.getMessage("player-kicked", brandName));
+    }
+
+    private void leaveBrand() {
+        Brand brand = Util.getPlayerBrand(player);
+        if (brand == null) {
+            player.sendMessage(Util.getMessage("must-have-brand", ""));
+            return;
+        }
+
+        String brandName = brand.getBrandName();
+
+        if (isBrandOwner(brand)) {
+            player.sendMessage(Util.getMessage("must-delete-brand", brandName));
+            return;
+        }
+
+        if (Util.hasBrand(player)) {
+            Player brandOwner = brand.getBrandOwner();
+            brand.removeMember(player);
+            player.sendMessage(Util.getMessage("brand-leave", brandName));
+            brandOwner.sendMessage(Util.getMessage("player-left-brand", brandName, player));
+        }
+
+    }
+
+    private void denyRequest() {
+        InviteRequest request = getRequest(player);
+        String brandName = Util.getPlayerBrand(player).getBrandName();
+
+        if (request != null) {
+            request.getSender().sendMessage(Util.getMessage("brand-deny-sender", player));
+            request.getReceiver().sendMessage(Util.getMessage("brand-deny", brandName));
+            inviteRequests.remove(request);
+            return;
+        }
+        player.sendMessage(Util.getMessage("no-requests", ""));
+    }
+
+    private void changeBrandSlogan(Brand brand, String slogan) {
+        if (brand == null) {
+            player.sendMessage(Util.getMessage("must-have-brand"));
+            return;
+        }
+
+        if (!isBrandOwner(brand)) {
+            player.sendMessage(Util.getMessage("must-own-brand"));
+            return;
+        }
+
+        if (slogan == null) {
+            player.sendMessage(Util.getMessage("must-specify-slogan"));
+        }
+
+        brand.setSlogan(slogan);
+        player.sendMessage(Util.getMessage("slogan-changed", brand.getBrandName()));
+    }
+
+    private boolean isBrandOwner(Brand brand) {
+        if (Util.hasBrand(player)) {
+            return brand.getBrandOwner().equals(player);
+        }
+        return false;
+    }
 
     private InviteRequest getRequest(Player receiver) {
         for (InviteRequest request : inviteRequests) {
@@ -215,16 +276,9 @@ public class BrandCommand implements CommandExecutor {
         return null;
     }
 
-    private boolean isBrandOwner(Player player, Brand brand) {
-        try {
-            return brand.getBrandOwner().equals(player);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
+    private String extractMultiWordString(String[] args) {
+        String[] arrayWithoutFirstArgument = Arrays.copyOfRange(args, 1, args.length);
+        return String.join(" ", arrayWithoutFirstArgument);
     }
 
-    private String[] removeFirstArgument(String[] args) {
-        return Arrays.copyOfRange(args, 1, args.length);
-    }
 }
