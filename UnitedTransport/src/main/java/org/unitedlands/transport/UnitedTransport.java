a1fc59d4332b00cb1998c3d6472aeff427e60689
Maroon28 <sbrushsize9@gmail.com>
2022-07-27 07:06:18 +0200

    UnitedTransport - Minor Code cleanup.

--- UnitedTransport/src/main/java/org/unitedlands/transport/UnitedTransport.java
+++ UnitedTransport/src/main/java/org/unitedlands/transport/UnitedTransport.java
@@ -4,15 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.unitedlands.transport.listeners.HorseListener;
 
 public final class UnitedTransport extends JavaPlugin {
-
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new HorseListener(this), this);
         saveDefaultConfig();
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
     }
 }
--- UnitedTransport/src/main/java/org/unitedlands/transport/listeners/HorseListener.java
+++ UnitedTransport/src/main/java/org/unitedlands/transport/listeners/HorseListener.java
@@ -1,10 +1,8 @@
 package org.unitedlands.transport.listeners;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.attribute.Attribute;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,26 +15,18 @@ import org.spigotmc.event.entity.EntityMountEvent;
 import org.unitedlands.transport.UnitedTransport;
 
 public class HorseListener implements Listener {
-    private static final FileConfiguration configuration;
     private final UnitedTransport unitedTransport;
 
-    static {
-        configuration = getConfiguration();
-    }
 
     public HorseListener(UnitedTransport unitedTransport) {
         this.unitedTransport = unitedTransport;
-    }
-
-    public static FileConfiguration getConfiguration() {
-        return Bukkit.getServer().getPluginManager().getPlugin("UnitedTransport").getConfig();
     }
 
     @EventHandler
     public void onHorseMount(EntityMountEvent event) {
         if (event.getMount() instanceof Horse horse) {
             if (!hasSpeedKey(horse)) {
-                saveHorseSpeed(horse);
+                saveBaseHorseSpeed(horse);
             }
         }
     }
@@ -48,15 +38,15 @@ public class HorseListener implements Listener {
         if (!player.isInsideVehicle()) return;
         if (player.getVehicle() instanceof Horse horse) {
             double horseSpeed = horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).getBaseValue();
-            double newHorseSpeed = getHorseSpeed(horse);
+            double newHorseSpeed = getBoostedHorseSpeed(horse);
             if (horseSpeed != newHorseSpeed) {
                 horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(newHorseSpeed);
             }
         }
     }
 
-    private double getHorseSpeed(Horse horse) {
-        for (String entry : configuration.getStringList("horse-speed-modifier")) {
+    private double getBoostedHorseSpeed(Horse horse) {
+        for (String entry : unitedTransport.getConfig().getStringList("horse-speed-modifier")) {
             String[] split = entry.split(";");
             String materialName = split[0];
             if (!getPathMaterial(horse).toString().equals(materialName)) {
@@ -78,7 +68,7 @@ public class HorseListener implements Listener {
         return 0;
     }
 
-    private void saveHorseSpeed(Horse horse) {
+    private void saveBaseHorseSpeed(Horse horse) {
         NamespacedKey speedKey = getSpeedKey();
         PersistentDataContainer container = horse.getPersistentDataContainer();
         if (!hasSpeedKey(horse)) {
@@ -98,7 +88,6 @@ public class HorseListener implements Listener {
     }
 
     private Material getPathMaterial(Horse horse) {
-        Material type = horse.getLocation().getBlock().getRelative(0, -1, 0).getType();
-        return type;
+        return horse.getLocation().getBlock().getRelative(0, -1, 0).getType();
     }
 }
