a497e5c15617be6b930cad4501253ece24e8961e
maroon28 <sbrushsize9@gmail.com>
2022-12-08 08:50:30 +0200

    Add 3 daily lives per resident, max of 6.

--- UnitedWars/src/main/java/org/unitedlands/wars/war/health/WarHealTask.java
+++ UnitedWars/src/main/java/org/unitedlands/wars/war/health/WarHealTask.java
@@ -1,6 +1,8 @@
 package org.unitedlands.wars.war.health;
 
+import com.palmergames.bukkit.towny.object.Resident;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.unitedlands.wars.war.WarDataController;
 import org.unitedlands.wars.war.WarDatabase;
 import org.unitedlands.wars.war.entities.WarringEntity;
 
@@ -11,6 +13,11 @@ public class WarHealTask extends BukkitRunnable {
         for (WarringEntity warringEntity : WarDatabase.getWarringEntities()) {
             int amount = warringEntity.getOnlinePlayers().size();
             warringEntity.getWarHealth().increaseHealth(amount);
+            // Add 3 lives for each resident, up to a max of 6.
+            for (Resident resident: warringEntity.getWarParticipants()) {
+                int currentLives = WarDataController.getResidentLives(resident);
+                WarDataController.setResidentLives(resident,Math.max(6, currentLives + 3));
+            }
         }
 
     }
