14d56cc4f6b8cfd86d3247ec34c13b98502144f6
maroon28 <sbrushsize9@gmail.com>
2022-12-22 05:11:19 +0200

    token add, set, and remove commands

--- UnitedWars/src/main/java/org/unitedlands/wars/commands/WarAdminCommand.java
+++ UnitedWars/src/main/java/org/unitedlands/wars/commands/WarAdminCommand.java
@@ -13,10 +13,10 @@ import org.bukkit.command.TabExecutor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.unitedlands.wars.UnitedWars;
+import org.unitedlands.wars.Utils;
 import org.unitedlands.wars.war.War;
 import org.unitedlands.wars.war.WarDataController;
 import org.unitedlands.wars.war.WarDatabase;
-import org.unitedlands.wars.war.WarUtil;
 import org.unitedlands.wars.war.entities.WarringEntity;
 import org.unitedlands.wars.war.entities.WarringNation;
 import org.unitedlands.wars.war.entities.WarringTown;
@@ -26,15 +26,25 @@ import java.util.Collections;
 import java.util.List;
 
 public class WarAdminCommand implements TabExecutor {
-    private static final List<String> adminTabCompletes = Arrays.asList("end", "purge");
-    private static final List<String> optionsTabCompletes = Arrays.asList("town", "nation");
+    private static final List<String> MAIN_TAB_COMPLETES = Arrays.asList("end", "purge", "tokens");
+    private static final List<String> END_TAB_COMPLETES = Arrays.asList("town", "nation");
+    private static final List<String> TOKENS_TAB_COMPLETES = List.of("add", "remove", "set");
     private CommandSender sender;
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
+        if (args[0].equalsIgnoreCase("tokens")) {
+            if (args.length == 2) {
+                return TOKENS_TAB_COMPLETES;
+            }
+            if (args.length == 3) {
+                return BaseCommand.getTownyStartingWith(args[2], "t");
+            }
+        }
         if (args[0].equalsIgnoreCase("end")) {
             if (args.length == 2) {
-                return optionsTabCompletes;
+                return END_TAB_COMPLETES;
             }
             if (args.length == 3) {
                 if (args[1].equalsIgnoreCase("town")) {
@@ -47,7 +57,7 @@ public class WarAdminCommand implements TabExecutor {
                 return Collections.emptyList();
             }
         }
-        return args.length == 1 ? adminTabCompletes : Collections.emptyList();
+        return args.length == 1 ? MAIN_TAB_COMPLETES : Collections.emptyList();
     }
 
     @Override
@@ -56,6 +66,23 @@ public class WarAdminCommand implements TabExecutor {
         if (args[0].equalsIgnoreCase("purge")) {
             parsePurgeCommand();
             return true;
+        }
+        if (args[0].equalsIgnoreCase("tokens")) {
+            Town town = UnitedWars.TOWNY_API.getTown(args[2]);
+            int amount = Integer.parseInt(args[3]);
+            if (town == null) {
+                sender.sendMessage(Utils.getMessage("invalid-town"));
+                return true;
+            }
+            if (town.isNeutral()) {
+                sender.sendMessage("Town is neutral!");
+                return true;
+            }
+            switch (args[1]) {
+                case "add" -> parseAddTokensCommand(town, amount);
+                case "remove" -> parseRemoveTokensCommand(town, amount);
+                case "set" -> parseSetTokensCommand(town, amount);
+            }
         }
         if (args[0].equalsIgnoreCase("end")) {
             if (args[1].equalsIgnoreCase("town")) {
@@ -87,6 +114,21 @@ public class WarAdminCommand implements TabExecutor {
             }
         }
         return false;
+    }
+
+    private void parseSetTokensCommand(Town town, int amount) {
+        WarDataController.setTokens(town, amount);
+        sender.sendMessage(Component.text("Set " + town.getFormattedName() + " tokens to " + amount));
+    }
+
+    private void parseRemoveTokensCommand(Town town, int amount) {
+        WarDataController.setTokens(town, Math.max(0, WarDataController.getWarTokens(town) - amount));
+        sender.sendMessage(Component.text("Removed " + amount + " tokens from " + town.getFormattedName()));
+    }
+
+    private void parseAddTokensCommand(Town town, int amount) {
+        WarDataController.setTokens(town, WarDataController.getWarTokens(town) + amount);
+        sender.sendMessage(Component.text("Added " + amount + " tokens to " + town.getFormattedName()));
     }
 
     private void parsePurgeCommand() {
