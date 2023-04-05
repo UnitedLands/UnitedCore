43ed0b7ed16d2742ec989d26241fa2b5016b8dcf
maroon28 <sbrushsize9@gmail.com>
2023-04-05 23:27:01 +0200

    rewrite the command more

--- UnitedChat/src/main/java/org/unitedlands/unitedchat/commands/GradientCommand.java
+++ UnitedChat/src/main/java/org/unitedlands/unitedchat/commands/GradientCommand.java
@@ -1,6 +1,5 @@
 package org.unitedlands.unitedchat.commands;
 
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextReplacementConfig;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -15,88 +14,95 @@ import org.unitedlands.unitedchat.player.ChatPlayer;
 import static org.unitedlands.unitedchat.UnitedChat.getMessage;
 
 public class GradientCommand implements CommandExecutor {
-
-    @Nullable
-    private static ConfigurationSection getPresetSection() {
-        return UnitedChat.getPlugin().getConfig().getConfigurationSection("Presets");
-    }
+    private Player player;
+    private ChatPlayer chatPlayer;
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
 
-        if (!(sender instanceof Player player)) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+        this.player = (Player) sender;
+
+        if (!player.hasPermission("united.chat.gradient")) {
+            player.sendMessage(getMessage("no-perm"));
             return false;
         }
 
-        Component noPermMessage = getMessage("no-perm");
-        if (!player.hasPermission("united.chat.gradient")) {
-            player.sendMessage(noPermMessage);
-            return false;
-        }
-        ChatPlayer chatPlayer = new ChatPlayer(player.getUniqueId());
+        chatPlayer = new ChatPlayer(player.getUniqueId());
 
         if (args.length != 1) {
             player.sendMessage(getMessage("gradient-command"));
             return true;
         }
 
-        if (args[0].equalsIgnoreCase("info")) {
-            String gradient = chatPlayer.getGradient().replace(":", "\n- ");
-            TextReplacementConfig gradientPlaceholder = TextReplacementConfig.builder().match("<gradient>").replacement(gradient).build();
-            player.sendMessage(getMessage("current-gradient").replaceText(gradientPlaceholder));
-            return true;
-        }
-
-        if (args[0].equalsIgnoreCase("on")) {
-            if (!chatPlayer.isGradientEnabled()) {
-                chatPlayer.setGradientEnabled(true);
-                player.sendMessage(getMessage("gradient-on"));
-            } else {
-                player.sendMessage(getMessage("gradient-is-on"));
-            }
-            return true;
-        }
-
-        if (args[0].equalsIgnoreCase("off")) {
-            if (chatPlayer.isGradientEnabled()) {
-                chatPlayer.setGradientEnabled(false);
-                player.sendMessage(getMessage("gradient-off"));
-            } else {
-                player.sendMessage(getMessage("gradient-is-off"));
-            }
-            return true;
-        }
-
-        ConfigurationSection presets = getPresetSection();
-
-        if (presets.getString(args[0]) == null) {
-            player.sendMessage(getMessage("gradient-unknown-preset"));
-            return true;
-        }
-
-        if (presets.contains(args[0])) {
-            if (player.hasPermission("united.chat.gradient." + args[0])) {
-                String preset = presets.getString(args[0]);
-                chatPlayer.setGradient(preset);
-                player.sendMessage(getMessage("gradient-changed"));
-            } else {
-                player.sendMessage(noPermMessage);
-            }
-            return true;
-        }
-
-        if (args[0].contains("#")) {
-            if (player.hasPermission("unitedchat.gradient.all")) {
-                chatPlayer.setGradient(args[0]);
-                player.sendMessage(getMessage("gradient-changed"));
-                return true;
-            }
-        } else {
-            player.sendMessage(noPermMessage);
-            return true;
+        switch (args[0]) {
+            case "info" -> sendGradientInfo();
+            case "on" -> enableGradient();
+            case "off" -> disableGradient();
+            default -> setGradient(args[0]);
         }
 
         return false;
+    }
+
+    private void setGradient(String arg) {
+        if (getPresetSection().contains(arg)) {
+            setGradientPreset(arg);
+        }
+        if (arg.contains("#")) {
+            if (player.hasPermission("unitedchat.gradient.all")) {
+                chatPlayer.setGradient(arg);
+                player.sendMessage(getMessage("gradient-changed"));
+            }
+        } else {
+            player.sendMessage(getMessage("no-perm"));
+        }
+    }
+
+    private void setGradientPreset(String presetName) {
+        if (getPresetSection().getString(presetName) == null) {
+            player.sendMessage(getMessage("gradient-unknown-preset"));
+            return;
+        }
+
+        if (player.hasPermission("united.chat.gradient." + presetName)) {
+            String preset = getPresetSection().getString(presetName);
+            chatPlayer.setGradient(preset);
+            player.sendMessage(getMessage("gradient-changed"));
+        } else {
+            player.sendMessage(getMessage("no-perm"));
+        }
+    }
+
+    private void disableGradient() {
+        if (chatPlayer.isGradientEnabled()) {
+            chatPlayer.setGradientEnabled(false);
+            player.sendMessage(getMessage("gradient-off"));
+        } else {
+            player.sendMessage(getMessage("gradient-is-off"));
+        }
+    }
+
+    private void enableGradient() {
+        if (!chatPlayer.isGradientEnabled()) {
+            chatPlayer.setGradientEnabled(true);
+            player.sendMessage(getMessage("gradient-on"));
+        } else {
+            player.sendMessage(getMessage("gradient-is-on"));
+        }
+    }
+
+    private void sendGradientInfo() {
+        String gradient = chatPlayer.getGradient().replace(":", "\n- ");
+        TextReplacementConfig gradientPlaceholder = TextReplacementConfig.builder().match("<gradient>").replacement(gradient).build();
+        player.sendMessage(getMessage("current-gradient").replaceText(gradientPlaceholder));
+    }
+
+    @Nullable
+    private ConfigurationSection getPresetSection() {
+        return UnitedChat.getPlugin().getConfig().getConfigurationSection("Presets");
     }
 
 }
