e824ea416296164634e6e8cb116dcdd6efd84e87
maroon28 <sbrushsize9@gmail.com>
2023-05-25 05:16:23 +0300

    add a get message method with placeholder support

--- UnitedChat/src/main/java/org/unitedlands/unitedchat/UnitedChat.java
+++ UnitedChat/src/main/java/org/unitedlands/unitedchat/UnitedChat.java
@@ -2,14 +2,12 @@ package org.unitedlands.unitedchat;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.unitedlands.unitedchat.commands.ClearChatCommand;
 import org.unitedlands.unitedchat.commands.GradientCommand;
 import org.unitedlands.unitedchat.player.PlayerListener;
-
-import java.io.File;
 
 public class UnitedChat extends JavaPlugin {
 
@@ -39,5 +37,14 @@ public class UnitedChat extends JavaPlugin {
     public static Component getMessage(String name) {
         String message = PLUGIN.getConfig().getString("messages." + name);
         return MiniMessage.miniMessage().deserialize(message);
+    }
+
+    public static Component getMessage(String message, TagResolver.Single... resolvers) {
+        String prefix = PLUGIN.getConfig().getString("messages.prefix");
+        String configuredMessage = PLUGIN.getConfig().getString("messages." + message);
+        if (configuredMessage == null) {
+            return MiniMessage.miniMessage().deserialize("<red>Message <yellow>" + message + "<red> could not be found in the config file!");
+        }
+        return MiniMessage.miniMessage().deserialize(prefix + configuredMessage, resolvers);
     }
 }
