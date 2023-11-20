3b11bc25858163e5c9328ce156e5157060e1683b
squeaky2137 <73867896+squeaky2137@users.noreply.github.com>
2023-11-19 18:48:25 -0600

    Update to Prod version and fix issue
    Fixed the issue where users couldnt break blocks under y-45 in trusted claims

--- UnitedProtection/src/main/java/org/unitedlands/protection/listeners/PlayerListener.java
+++ UnitedProtection/src/main/java/org/unitedlands/protection/listeners/PlayerListener.java
@@ -1,9 +1,11 @@
 package org.unitedlands.protection.listeners;
 
 import com.palmergames.bukkit.towny.TownyAPI;
-import com.palmergames.bukkit.towny.object.Resident;
-import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownBlock;
+import java.util.List;
+
+import com.palmergames.bukkit.towny.object.TownyPermission;
+import com.palmergames.bukkit.towny.utils.PlayerCacheUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
@@ -14,14 +16,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.unitedlands.protection.UnitedProtection;
-import org.unitedlands.wars.war.WarDatabase;
-
-import java.util.List;
 
 public class PlayerListener implements Listener {
-
     private final TownyAPI towny = TownyAPI.getInstance();
     private final UnitedProtection unitedProtection;
     private Player player;
@@ -32,86 +29,62 @@ public class PlayerListener implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        player = event.getPlayer();
+        this.player = event.getPlayer();
         Block block = event.getBlock();
-
-        if (isProtectedBlock(block)) {
-            player.sendMessage(getMessage("break-deny-message"));
+        if (this.isProtectedBlock(block)) {
+            this.player.sendMessage(this.getMessage("break-deny-message"));
             event.setCancelled(true);
         }
+
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
-        player = event.getPlayer();
+        this.player = event.getPlayer();
         Block block = event.getBlock();
-
-        if (isProtectedBlock(block)) {
-            player.sendMessage(getMessage("place-deny-message"));
+        if (this.isProtectedBlock(block)) {
+            this.player.sendMessage(this.getMessage("place-deny-message"));
             event.setCancelled(true);
         }
+
     }
 
     private int getProtectedY() {
-        return getConfiguration().getInt("protect-anything-below");
+        return this.getConfiguration().getInt("protect-anything-below");
     }
 
     private List<String> getWhitelistedBlocks() {
-        return getConfiguration().getStringList("block-whitelist");
+        return this.getConfiguration().getStringList("block-whitelist");
     }
 
-    @NotNull
-    private FileConfiguration getConfiguration() {
-        return unitedProtection.getConfig();
+    private @NotNull FileConfiguration getConfiguration() {
+        return this.unitedProtection.getConfig();
     }
 
     private boolean isWhitelistedBlock(Block block) {
-        return getWhitelistedBlocks().contains(block.getType().toString());
+        return this.getWhitelistedBlocks().contains(block.getType().toString());
     }
 
     private boolean isInTown(Block block) {
         Location blockLocation = block.getLocation();
-        if (towny.isWilderness(blockLocation)) {
-            return false;
-        }
-
-        Resident resident = towny.getResident(player);
-        @Nullable TownBlock townBlock = towny.getTownBlock(blockLocation);
+        if (this.towny.isWilderness(blockLocation)) return false;
+        TownBlock townBlock = this.towny.getTownBlock(blockLocation);
         if (townBlock == null) return false;
-        Town town = townBlock.getTownOrNull();
-        if (town == null || !resident.hasTown()) {
-            return false;
-        }
-        return resident.getTownOrNull().equals(town) || town.getTrustedResidents().contains(resident) || isInWarringTown(resident, town);
-    }
-
-    private boolean isInWarringTown(Resident resident, Town town) {
-        if (!WarDatabase.hasWar(town) || !WarDatabase.hasWar(resident.getPlayer()))
-            return false;
-        return WarDatabase.getWar(town) == WarDatabase.getWar(resident.getPlayer());
+        return PlayerCacheUtil.getCachePermission(this.player, blockLocation, block.getType(), TownyPermission.ActionType.DESTROY);
     }
 
     private String getMessage(String message) {
-        message = getConfiguration().getString(message);
+        message = this.getConfiguration().getString(message);
         return ChatColor.translateAlternateColorCodes('&', message);
     }
 
     private boolean isProtectedBlock(Block block) {
         int blockY = block.getLocation().getBlockY();
         String worldName = block.getWorld().getName();
+        if (!worldName.equals("world_earth")) return false;
+        if (this.isWhitelistedBlock(block)) return false;
+        if (this.player.hasPermission("united.protection.bypass")) return false;
 
-        if (!worldName.equals("world_earth")) {
-            return false;
-        }
-
-        if (isWhitelistedBlock(block)) {
-            return false;
-        }
-
-        if (player.hasPermission("united.protection.bypass")) {
-            return false;
-        }
-
-        return blockY < getProtectedY() && !isInTown(block);
+        return blockY < this.getProtectedY() && !this.isInTown(block);
     }
 }
